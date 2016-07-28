179b29c98e63ecbedecc07a702370f7ba0e764f6
CharlyZhang <chicboi@hotmail.com>
2016-07-28 14:59:10 +0800

    initial commit

---
+++ BrushAdjust/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BrushAdjust
+//
+//  Created by CharlyZhang on 16/7/28.
+//  Copyright © 2016年 CharlyZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BrushAdjust/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BrushAdjust
+//
+//  Created by CharlyZhang on 16/7/28.
+//  Copyright © 2016年 CharlyZhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
